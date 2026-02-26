90b1837aee7c23e8673db9e4935feb19beb57854
TanishaDutta-106 <tanisha77@usf.edu>
2026-02-25 19:31:01 -0500

    mcp main.py

--- src/mcp/main.py
+++ src/mcp/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from mcp.routes.business_routes import router as business_router
+from mcp.routes.inventory_routes import router as inventory_router
 
 app = FastAPI()
 
 app.include_router(business_router)
+app.include_router(inventory_router)
